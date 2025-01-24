d36224938d83304addb3e8e3ff9e5c418e78e146
Víctor Quiceno <vaquiceno@javerianacali.edu.co>
2025-01-23 23:05:29 -0500

    create exceptionhandler

---
+++ src/main/java/com/dashboard/app/handlers/CustomExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.dashboard.app.handlers;
+
+import com.dashboard.app.exception.AppException;
+import com.dashboard.app.response.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class CustomExceptionHandler {
+    @ExceptionHandler(AppException.class)
+    public ResponseEntity<ErrorResponse> handleAppException(AppException e) {
+        ErrorResponse errorResponse = new ErrorResponse(e.getMessage(), HttpStatus.BAD_REQUEST.value());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        ErrorResponse errorResponse = new ErrorResponse(e.getMessage(), HttpStatus.BAD_REQUEST.value());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+}
+
+
---
+++ src/main/java/com/dashboard/app/response/ErrorResponse.java
@@ -0,0 +1,19 @@
+package com.dashboard.app.response;
+
+public class ErrorResponse {
+    private String message;
+    private int statusCode;
+
+    public ErrorResponse(String message, int statusCode) {
+        this.message = message;
+        this.statusCode = statusCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+}
